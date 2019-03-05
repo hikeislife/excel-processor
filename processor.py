dbcffb369c58c4d880fe4b6c6ebb4b92ce828435
hikeislife <hikeislife@users.noreply.github.com>
2019-03-05 23:16:58 +0100

    generalized code

--- processor.py
+++ processor.py
@@ -3,9 +3,10 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
+def processWB (filename):
+    wb = xl.load_workbook('filename')
 
-sheet = wb['Sheet1']
+    sheet = wb['Sheet1']
 
 # cell = sheet['a1']
 
@@ -15,25 +16,25 @@ sheet = wb['Sheet1']
 
 # print(sheet.max_row)
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    # print(cell.value)
-    correctedPrice = cell.value * .9
-    correctedCell = sheet.cell(row, 4)
-    correctedCell.value = correctedPrice
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        # print(cell.value)
+        correctedPrice = cell.value * .9
+        correctedCell = sheet.cell(row, 4)
+        correctedCell.value = correctedPrice
 
-values = Reference(sheet,
-                   min_row=2,
-                   max_row=sheet.max_row,
-                   min_col=4,
-                   max_col=4
-                   )
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4
+                       )
 
-chart = BarChart()
-chart.add_data(values)
+    chart = BarChart()
+    chart.add_data(values)
 
-sheet.add_chart(chart, 'E2')
+    sheet.add_chart(chart, 'E2')
 
-wb.save('transactions2.xlsx')
+    wb.save(filename)
 
 
