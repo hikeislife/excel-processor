b6f3190d5e5f41baa314a5586d6475bceba17d46
hikeislife <hikeislife@users.noreply.github.com>
2019-03-05 23:08:06 +0100

    correction e2 to E2

--- processor.py
+++ processor.py
@@ -26,12 +26,13 @@ values = Reference(sheet,
                    min_row=2,
                    max_row=sheet.max_row,
                    min_col=4,
-                   max_col=4)
+                   max_col=4
+                   )
 
 chart = BarChart()
 chart.add_data(values)
 
-sheet.add_chart(chart, 'e2')
+sheet.add_chart(chart, 'E2')
 
 wb.save('transactions2.xlsx')
 
